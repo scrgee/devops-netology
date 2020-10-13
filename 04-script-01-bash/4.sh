886154f67107fc7a3bffdced539a3acb829d55d1
Baurzhan Zhumabayev <jumabaev.baurzhan@gmail.com>
2020-10-13 15:07:20 +0600

    add comments

--- 04-script-01-bash/4.sh
+++ 04-script-01-bash/4.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
-counter=1
-log_file="/tmp/port_checking.log"
-if [ ! -f "$log_file" ]
-then
-  truncate -s 0 "$log_file"
-else
-  touch "$log_file"
-fi
-
-while [ $counter -le 5 ]
+IP_list=( "192.168.0.1" "173.194.222.113" "87.250.250.242" ) #Массив из адресов
+while  (( 1 == 1 )) #Бесконечный цикл
 do
-  counter=$(( counter + 1 ))
-  nc -zv 192.168.0.1 80 >> $log_file 2>&1
-  nc -zv 173.194.222.113 80 >> $log_file 2>&1
-  nc -zv 87.250.250.242 80 >> $log_file 2>&1
+  for IP in "${IP_list[@]}"  #Цикл проходит каждый адрес в массиве IP_list
+  do
+    nc -zvw3 "${IP}" 80 > /dev/null 2>&1
+    if  [ $? != 0 ]
+    then
+      echo "${IP}" >> error.log
+      a=1 #Переменная флаг, для условия выхода из скрипта
+    fi
+  done
+
+  if (( a == 1 )) #Условие выхода из скрипта
+  then
+    exit 1
+  fi
 done
